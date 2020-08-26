4ba2cde8db17ad87c80c2a623a84067349e43fad
Leeepsa <65106421+Leeepsa@users.noreply.github.com>
2020-08-26 20:53:21 +0530

    Add files via upload

---
+++ practice.R
@@ -0,0 +1,48 @@
+
+1+1
+2+3*4
+exp(1)
+pi
+x <- 58
+y<-20
+z<-10
+1+1
+
+(6*3)+ 5/2
+
+#create a vector
+x <- c(12,20,15,100)
+my_vect <- c(12,20,15,100)
+
+vect_x<-c(2,0,0,4)
+vect_y <- c(1,9,9,9)
+
+#add the vectors
+vect_add <- vect_x+vect_y
+
+#Mul vect_x with some scalar
+vect_x*5
+#Accessing the elements of the vector
+vect_x[1]
+vect_x[4]
+ 
+vect_x[2]<-2
+
+vect_x
+
+vect_char <- c('R', 'python', 'java', 10, 100.5)
+
+vect_x<-c(2,0,0,4)
+vect_y <- c(1,9,9,9)
+
+vect_add <- vect_x+vect_y
+
+chaar_v <- c("a","b","c")
+
+df<-data.frame(x=c(1,2,3),y=c("a","b","c"))
+
+Df_new<-data.frame(height=c(150,160),weight=c(65,72))
+
+
+cust[1,2,2]
+               
